44d635ddd58b4e1c01f4ecea39a9c11d83127643
LatenLavier <mxlaten@gmail.com>
2021-11-01 17:31:01 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiurl: 'https://twiliapi.herokuapp.com',
+    apipassword: 'tbzyOL1aevCuxmrTfEctI9AiTcjRZ6sG',
+
+    discordtoken: 'ODg3NDkwOTY2NDc3NDg0MDYy.YUE6Xw.PAB0kfCmNLOuLQqegXDKh4Ep_Gw',
+    discordprefix: '!',
+    secretpassword: 'ChAnGeThIsWiThYoUrPaSsWoRdNoW!',
+    botuser_rolename: 'Bot User',
+    admin_rolename: 'Admin'
+};
