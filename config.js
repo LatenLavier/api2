6254147a16a4bd0cb2ad7bbdf0dfde08981b6037
LatenLavier <83513405+LatenLavier@users.noreply.github.com>
2021-11-01 17:40:36 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     apiurl: 'https://twiliapi.herokuapp.com',
     apipassword: 'tbzyOL1aevCuxmrTfEctI9AiTcjRZ6sG',
 
-    discordtoken: 'ODg3NDkwOTY2NDc3NDg0MDYy.YUE6Xw.PAB0kfCmNLOuLQqegXDKh4Ep_Gw',
+    discordtoken: 'ODg3NDkwOTY2NDc3NDg0MDYy.YUE6Xw.fUvGtHt11PDPLeL0oZ5iFDYp9hw',
     discordprefix: '!',
     secretpassword: 'ChAnGeThIsWiThYoUrPaSsWoRdNoW!',
     botuser_rolename: 'Bot User',
